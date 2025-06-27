5d6a02aed2a31a9a1d99a0a755bb102bf00cfba1
Dilip Maurya <143182090+dil-12345@users.noreply.github.com>
2025-06-27 16:11:47 +0530

    stripe add

--- server/server.js
+++ server/server.js
@@ -3,23 +3,36 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinay from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
 
+//initialize 
 const app = express();
+
+//data base conection
 await connectDB();
+await connectCloudinay();
+
+//middlewares
 app.use(cors());
+app.use(clerkMiddleware())
 
 
 //Routs
-
 app.get('/', (req, res) => {
     res.send("API  Working");
 });
+app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter);
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
-app.post('/clerk', express.json(), clerkWebhooks);
-
-
-
+//PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
